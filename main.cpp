0a74dc3f7b6febc9d2d3d032e86a6ae3de58a5d4
Konstantinos Karatsenidis <sdi1300064@di.uoa.gr>
2016-10-15 21:40:57 +0300

    mia mikrh arxh

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#define N 3
+#define OK_SUCCESS 0
+#include <cstdio>
+
+typedef unsigned int long uint32_t;
+
+/*
+NodeIndex* createNodeIndex();
+OK_SUCCESS insertNode(NodeIndex*, uint32_t nodeId,.. );
+size_t getListHead( NodeIndex*, uint32_t nodeId );
+OK_SUCCESS destroyNodeIndex( NodeIndex* );
+*/
+
+struct list_node {
+	uint32_t neighbor[N]; //the ids of the neighbor nodes
+	uint32_t edgeProperty[N]; //property for each edge
+	size_t nextListNode; //
+};
+
+class Buffer {
+	size_t size;
+	size_t current;
+public:
+	Buffer(); //Buffer* createBuffer();
+	~Buffer(); //OK_SUCCESS destroyBuffer(Buffer*);
+	size_t allocNewNode();
+	list_node* getListNode(size_t);
+};
+
+class NodeIndex {
+	size_t size;
+	size_t current;
+public:
+	NodeIndex(); //NodeIndex* createNodeIndex();
+	~NodeIndex(); //OK_SUCCESS destroyNodeIndex( NodeIndex* );
+	OK_SUCCESS insertNode(NodeIndex*, uint32_t nodeId, ...);
+	size_t getListHead( NodeIndex*, uint32_t nodeId );
+};
+
+int main(int argc, char const *argv[]) {
+	uint32_t test = 0x00ffffff;
+	printf("%lu\n", test);
+	printf("%lu\n", sizeof(test));
+	return 0;
+}
