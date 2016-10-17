c18b012015a9b07e9560bbb4f476c4159d109607
Konstantinos Karatsenidis <karatsenidis@outlook.com>
2016-10-17 15:47:50 +0300

    graph.h

---
+++ Graph.h
@@ -0,0 +1,26 @@
+#ifndef __GRAPH_H__
+#define __GRAPH_H__
+
+class Pair {
+	Buffer buffer;
+	NodeIndex index;
+public:
+	Pair();
+	~Pair();
+	createNode(uint32_t id);
+	addEdge(uint32_t from, uint32_t to);
+};
+
+class Graph
+{
+	Pair in;
+	Pair out;
+public:
+	Graph();
+	~Graph();
+	createNode(uint32_t id);
+	addEdge(uint32_t from, uint32_t to);
+	question(uint32_t from, uint32_t to);
+};
+
+#endif
