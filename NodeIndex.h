c5895b40957f37eea0b04b72f0e329b2cd624dfd
Konstantinos Karatsenidis <karatsenidis@outlook.com>
2016-10-17 15:49:36 +0300

    NodeIndex.h

---
+++ NodeIndex.h
@@ -0,0 +1,15 @@
+#ifndef __NODEINDEX_H__
+#define __NODEINDEX_H__
+
+class NodeIndex {
+	size_t size; //current size of capacity used
+	size_t capacity; //capacity of table
+	size_t* table; //offset of Buffer line
+public:
+	NodeIndex(); //NodeIndex* createNodeIndex();
+	~NodeIndex(); //OK_SUCCESS destroyNodeIndex(NodeIndex*);
+	bool insertNode(NodeIndex*, uint32_t nodeId, ...);
+	size_t getListHead(NodeIndex*, uint32_t nodeId);
+};
+
+#endif
