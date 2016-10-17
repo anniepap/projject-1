21bf35a8b276ef7ea700271275d8d29d3edbfa70
Konstantinos Karatsenidis <karatsenidis@outlook.com>
2016-10-17 15:51:24 +0300

    Buffer.h

---
+++ Buffer.h
@@ -0,0 +1,15 @@
+#ifndef __BUFFER_H__
+#define __BUFFER_H__
+
+class Buffer {
+	size_t size; //current size of capacity used
+	size_t capacity; //capacity of table
+	list_node* table; //offset of Buffer line
+public:
+	Buffer(); //Buffer* createBuffer();
+	~Buffer(); //OK_SUCCESS destroyBuffer(Buffer*);
+	size_t allocNewNode();
+	list_node* getListNode(size_t);
+};
+
+#endif
