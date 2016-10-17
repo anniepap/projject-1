14f2ffceec334dc6ed73eb0467f040a3cb12b37d
Konstantinos Karatsenidis <karatsenidis@outlook.com>
2016-10-17 15:53:36 +0300

    ListNode.h

---
+++ ListNode.h
@@ -0,0 +1,12 @@
+#ifndef __LIST_NODE_H__
+#define __LIST_NODE_H__
+
+class list_node {
+	size_t size; //current size of capacity used (N is the capacity)
+	//size_t capacity = N;
+	uint32_t neighbor[N]; //the ids of the neighbor nodes //offset of NodeIndex line
+	uint32_t edgeProperty[N]; //property for each edge
+	size_t nextListNode; //0 means no overflow
+};
+
+#endif
