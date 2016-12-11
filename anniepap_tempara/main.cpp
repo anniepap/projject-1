aad9a045327259821da5bbe0e65d160a8b51f20f
anniepap <ann.papah@gmail.com>
2016-12-11 22:38:05 +0200

    forgot main...

---
+++ anniepap_tempara/main.cpp
@@ -0,0 +1,45 @@
+#include "Graph.h"
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+	//if (argc != 3) return -1;
+
+	Graph graph;
+	ifstream myReadFile;
+	char com;
+	uint32_t from;
+	uint32_t to;
+
+	myReadFile.open(argv[1]);
+	if (myReadFile.is_open()) {
+		while (!myReadFile.eof()) {
+			myReadFile >> from >> to;
+			if (!myReadFile.good())
+				break;
+			graph.addEdge(from, to);
+		}
+	}
+	myReadFile.close();
+
+	graph.test();
+
+/*
+	myReadFile.open(argv[2]);
+	if (myReadFile.is_open()) {
+		while(!myReadFile.eof()) {
+			myReadFile >> com;
+			if (com != 'F')
+				myReadFile >> from >> to;
+			if (com == 'A')
+				graph.addEdge(from, to);
+			if (com == 'Q')
+				cout << graph.question(from, to) << endl;
+//				graph.question(from, to);
+		}
+	}
+	myReadFile.close();
+*/
+	return 0;
+}
