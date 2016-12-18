822a9a0eaeb5d62d4e4789034dce3ab163f34c85
Geotha <geotha1995@gmail.com>
2016-12-18 15:34:30 +0200

    Katharisa tin main gia na tin vleoume kalutera

--- Part2/main.cpp
+++ Part2/main.cpp
@@ -12,10 +12,8 @@ int main(int argc, char** argv) {
 	
 	Graph graph;
 	ifstream myReadFile;
-	char com;
-	//char* gType = new char[10];
 	string graphType;
-	//int size = 0;
+	char com;
 	uint32_t from;
 	uint32_t to;
 
@@ -29,32 +27,18 @@ int main(int argc, char** argv) {
 		}
 	}
 	myReadFile.close();
-
+ 	
 	// Read Type
-	/*		Thelei ftiaksimo
 	myReadFile.open(argv[2]);
 	if (!myReadFile.is_open())
 		cerr<<"File can not open"<<endl;
 
 	getline(myReadFile, graphType);
 	
-	//myReadFile.getline(gType, sizeof(gType)*10);
-
-	//for (int i = 0; gType[i] != '\n'; ++i) {
-	//	size++;
-	//}
-
-	//char* graphType = new char[size];
-	*/
-
-	//for (int i = 0; i < size; ++i) {
-	//	graphType[i] = gType[i];
-	//}
-	//graphType[size] = '\0';
+	cout<<"|"<<graphType<<"|"<<endl;
 
 	// Check Type
 	if (graphType == "STATIC") {
-		cout<<"STATIC"<<endl;
 		SCC components( graph.SizeOfNodes(), graph );
 		GrailIndex grail_index(&components);	
 
@@ -77,17 +61,17 @@ int main(int argc, char** argv) {
 	}
 
 	else if (graphType == "DYNAMIC") {
-		cout<<"DYNAMIC"<<endl;
-		return 0;
 		CC cc( graph.SizeOfNodes(), &graph );
 
 		myReadFile.open(argv[2]);
 		if (myReadFile.is_open()) {
 			while(!myReadFile.eof()) {
 				myReadFile >> com;
+				cout<<com<<endl;
+				return 0;
 				if (com != 'F')
 					myReadFile >> from >> to;
-				if ( com == 'A'){
+				if (com == 'A'){
 					graph.addEdge( from, to );
 					cc.insertNewEdge( from, to ); 
 				}
@@ -101,20 +85,6 @@ int main(int argc, char** argv) {
 		}
 		myReadFile.close();
 	}
-	
-	//graph.test();
-
-	//delete[] gType;
-	//delete[] graphType;
 
 	return 0;
 }
-
-
-/*
-leipoun 
-1) hypergraph
-2) CC
-3) Anazitisi sto idio component
-4) Anazitisi se diaforetika component alla me elegxo kathe fora
-*/ 
